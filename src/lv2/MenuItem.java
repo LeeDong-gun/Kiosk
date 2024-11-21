68079d054e7888e2e58d762710eb4c0b045dc3b9
ehdrjs4802 <ehdrjs4802@gmail.com>
2024-11-21 21:45:53 +0900

    :sparkles: final

---
+++ src/lv2/MenuItem.java
@@ -0,0 +1,13 @@
+package lv2;
+
+public class MenuItem {
+    String burgerName;
+    double price;
+    String comment;
+
+    public MenuItem (String burgerName, double price, String comment) {
+        this.burgerName = burgerName;
+        this.price = price;
+        this.comment = comment;
+    }
+}
